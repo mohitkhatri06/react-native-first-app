6d4147cceb3f6fef07f2fb2ec48c7a0b8814b3d3
Mohit <mohitkhatri4272@gmail.com>
2022-12-01 13:06:19 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.dummyText}>This is text1</Text>
+      <Text style={styles.dummyText}>Open up App</Text>
+      <Button title='Tab me'/>
     </View>
   );
 }
@@ -16,5 +17,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  dummyText:{
+      margin:16, 
+      borderWidth:2, 
+      borderColor:'blue', 
+      padding:10,
   },
 });
